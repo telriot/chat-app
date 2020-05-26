aaed25de2e25df2ef210e4e682e181c37395e5b1
Benjo <beniamino.tartarini@gmail.com>
2020-05-26 12:17:20 +0900

    add production build settings to app.js

--- app.js
+++ app.js
@@ -36,22 +36,6 @@ app.use(cookieParser())
 app.use("/api", indexRouter)
 app.use("/api/users", usersRouter)
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404))
-})
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message
-  res.locals.error = req.app.get("env") === "development" ? err : {}
-
-  // render the error page
-  res.status(err.status || 500)
-  res.render("error")
-})
-
 // Prepare Production Settings
 
 if (process.env.NODE_ENV === "production") {
